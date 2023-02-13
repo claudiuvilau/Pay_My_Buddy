2eeba542d9bda37add8e1154d034d5e77ea956b8
Claudiu Vilau <vilau_claudiu@yahoo.com>
2023-02-13 22:42:15 +0100

    update applicatopn propreties - obsolete MySQL5Dialect Add Service package change the name of table FriendsNetwork in frendsnetwork if not the java think frends_network

--- src/main/java/com/openclassrooms/pay_my_buddy/PayMyBuddyApplication.java
+++ src/main/java/com/openclassrooms/pay_my_buddy/PayMyBuddyApplication.java
@@ -1,13 +1,39 @@
 package com.openclassrooms.pay_my_buddy;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.openclassrooms.pay_my_buddy.model.Users;
+import com.openclassrooms.pay_my_buddy.service.UsersService;
+
 @SpringBootApplication
-public class PayMyBuddyApplication {
+public class PayMyBuddyApplication implements CommandLineRunner {
+
+	@Autowired
+	private UsersService usersService; // instance of object
 
 	public static void main(String[] args) {
 		SpringApplication.run(PayMyBuddyApplication.class, args);
+	}
+
+	// the result of implemantation of interface CommandLineRunner
+	// it run everytime when the application start
+	@Override
+	public void run(String... args) throws Exception {
+
+		Iterable<Users> users = usersService.getUsers();
+		users.forEach(user -> System.out.println(user.getNameUser()));
+
+		Optional<Users> optProduct = usersService.getUserById(2);
+		Users userId = optProduct.get();
+		System.out.println(userId.getFirstName());
+
+		userId.getFriendsNetworks().forEach(
+				friend -> System.out.println(friend.getBuddy()));
 	}
 
 }
--- src/main/java/com/openclassrooms/pay_my_buddy/model/FriendsNetwork.java
+++ src/main/java/com/openclassrooms/pay_my_buddy/model/FriendsNetwork.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "FriendsNetwork")
+@Table(name = "friendsnetwork")
 public class FriendsNetwork {
 
     @Id
