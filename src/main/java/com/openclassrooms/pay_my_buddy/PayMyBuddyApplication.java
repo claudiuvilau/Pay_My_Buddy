6476709ca6abb8990a7d49dbfc2a9ad16ae08b27
Claudiu Vilau <vilau_claudiu@yahoo.com>
2023-02-21 08:25:34 +0100

    not working

--- src/main/java/com/openclassrooms/pay_my_buddy/PayMyBuddyApplication.java
+++ src/main/java/com/openclassrooms/pay_my_buddy/PayMyBuddyApplication.java
@@ -3,7 +3,6 @@ package com.openclassrooms.pay_my_buddy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
 @ComponentScan("com.openclassrooms.pay_my_buddy.controller")
