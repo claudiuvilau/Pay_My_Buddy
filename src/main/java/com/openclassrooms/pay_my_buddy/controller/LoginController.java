9bd196a07392608409f2d9386ca47ab26382f4a1
Claudiu Vilau <vilau_claudiu@yahoo.com>
2023-03-09 22:46:26 +0100

    simulation de l'ajout sur l'affichage de la page

--- src/main/java/com/openclassrooms/pay_my_buddy/controller/LoginController.java
+++ src/main/java/com/openclassrooms/pay_my_buddy/controller/LoginController.java
@@ -1,13 +1,18 @@
 package com.openclassrooms.pay_my_buddy.controller;
 
 import java.security.Principal;
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -66,6 +71,25 @@ public class LoginController {
     @RequestMapping("/add_connection")
     public ModelAndView addConnection() {
         modelAndView.setViewName("add_connection.html");
+
+        return modelAndView;
+    }
+
+    @RolesAllowed("USER")
+    @RequestMapping("/addedconnection")
+    public ModelAndView addedConnection(Model model, Principal user, @RequestParam(required = true) String nom,
+            @RequestParam(required = true) String prenom, @RequestParam(required = true) Date dateDeNaissance,
+            @RequestParam(required = true) String email) {
+
+        // récupérer ID email de la personne connectée et la personne connectée
+        String idEmail = getUserInfo(user);
+        Users nameUser = usersService.getUser(idEmail);
+        model.addAttribute("getUser", nameUser.getFirstName() + " " + nameUser.getNameUser());
+
+        modelAndView.setViewName("addedconnection.html");
+
+        model.addAttribute("addedConnection", "Vous venez d'ajouter : " + email);
+
         return modelAndView;
     }
 
